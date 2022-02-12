e89a80b2f660ce04b054532248f8971762c28c0c
shaktiv29 <shaktiv@jecjabalpur.ac.in>
2022-02-12 15:35:56 +0530

    Adding https

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getTempratureData = async (city) => {
   try {
